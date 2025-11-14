6ba86b2c85b42a6f4bc3b3d1120f77cd84fabb2d
Tristan-Tenders <tristangandi@gmail.com>
2025-11-14 16:24:50 +0100

    added userService

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 import express from "express"
 import { logMiddleware } from "./middleware/middleware.js"
-import { getAllUsers, getUserById, createUser,updateUser,deleteUser } from "./src/controllers/userController.js"
+import userRoutes from "./src/routes/userRoutes.js"  // Import the router
 
 const app = express()
 const PORT = 3000
 
+// Global middleware (applies to all routes)
 app.use(express.json())
 
-app.get("/users", logMiddleware, getAllUsers)
-app.get("/users/:id", getUserById)
-app.post("/users", createUser)
-app.put("/users/:id", updateUser)
-app.delete("/users/:id", deleteUser)
+// Mount the user router at /users
+app.use('/users', userRoutes)
+
+// Optional: Add a welcome route
+app.get('/', (req, res) => {
+	res.json({ 
+		message: "Welcome to the API",
+		endpoints: {
+			users: "/users"
+		}
+	})
+})
 
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}/users`)
+	console.log(`Server is running on http://localhost:${PORT}`)
+	console.log(`API Documentation:`)
+	console.log(`  GET    /users      - Get all users`)
+	console.log(`  GET    /users/:id  - Get user by ID`)
+	console.log(`  POST   /users      - Create new user`)
+	console.log(`  PUT    /users/:id  - Update user`)
+	console.log(`  DELETE /users/:id  - Delete user`)
 })
