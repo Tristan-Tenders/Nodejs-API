ee7e826b54e190f6f53056259885b3b4f5b82e29
Tristan Gandiboyina <Tristan.gandiboyina@epita.fr>
2025-11-14 14:27:07 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hello...")
+
+const userOne = {
+    firstName: "Bog",
+    age: 27,
+    admin: false
+}
+
+userOne.age = 10
+
+console.log(userOne)
---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express"
+import { logMiddleware } from "./middleware/middleware.js"
+
+
+const app = express()
+const PORT = 3000
+
+const users = [
+	{ id: 1, name: "Alice" },
+	{ id: 2, name: "Bob" },
+	{ id: 3, name: "Charlie" },
+	{ id: 4, name: "Dave" },
+]
+
+app.use(express.json())
+
+// middleware to log request body
+
+app.get("/", logMiddleware, (req, res) => {
+	res.json({ users,date })
+})
+
+app.get("/", (req, res) => {
+	res.json(users)
+})
+
+app.post("/", (req, res) => {
+	console.log(req)
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`)
+})
